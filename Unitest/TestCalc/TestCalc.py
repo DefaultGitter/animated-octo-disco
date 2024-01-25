9a4ad03d8fcacf32631e46381067dd516e8aed94
DefaultGitter <burlonepazzo@gmail.com>
2024-01-25 22:42:28 +0300

    pytest, lecture 36:29

--- Unitest/TestCalc/TestCalc.py
+++ Unitest/TestCalc/TestCalc.py
@@ -5,10 +5,10 @@ from Unitest.Calc.prog import calc
 class TestCalc(TestCase):
     def test_plus(self):
         # self.assertEqual(calc('2+2'), 4)
-        self.assertEqual(calc('2+2'), 5)
+        self.assertEqual(calc('2+2'), 4)
 
     def test_minus(self):
-        self.assertEqual(calc('5-2'), 1)
+        self.assertEqual(calc('5-2'), 3)
 
     def test_no_sign(self):
         with self.assertRaises(ValueError) as ex:
